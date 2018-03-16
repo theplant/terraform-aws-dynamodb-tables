acdb8cbfbc63a0eb429b73c47ba9d0536165bdb8
frankyue <frankyue1019@gmail.com>
2018-03-16 17:29:03 +0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "tables" {
   description = "The user and name of the dynamodb tables"
   value = ["${aws_dynamodb_table.table.*.name}"]
   value       =  {
-    buckets = "${var.table_info}"
+    table = "${var.table_info}"
     access_key_id = "${aws_iam_access_key.dynamodb.id}"
     secret_access_key = "${aws_iam_access_key.dynamodb.secret}"
   }
